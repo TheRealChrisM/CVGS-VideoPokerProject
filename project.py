739c1a469fdf7930f9945cfb7f7f7b86b856108e
Chris Marotta <marottachristopher@gmail.com>
2019-12-03 07:46:41 -0500

    Added program backbone

--- project.py
+++ project.py
@@ -2,3 +2,15 @@
 #December 3rd, 2019
 #Video Poker Project
 
+from tkinter import *
+#from PIL import ImageTk
+
+#Create Tkinter window variables
+window = Tk()
+#Create general variables
+
+#Create user class
+
+#create game class
+
+window.mainloop()
